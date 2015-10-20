b8127912138d482417ec2ebb7d8a2a2fcd7ae574
Zcience <david.liebemann@gmail.com>
2015-10-20 14:57:06 +0200

    Deleted old project, updated to new Gradle based project Well, I deleted it all. Wasn't much :D I used the libgdx project setup to make a completely new project, including all additional packages like box2d, ashley etc. It is now managed by Gradle.

--- Code/Z1/src/de/zcience/Z1/zengine/profiling/Profiling.java
+++
@@ -1,5 +0,0 @@
-package de.zcience.Z1.zengine.profiling;
-
-public interface Profiling {
-	public void setProfiling(boolean profiling);
-}
---
+++ desktop/src/de/zcience/zengine/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package de.zcience.zengine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.zcience.zengine.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "z1";
+		config.width=480;
+		config.height= config.width * 9 / 16;
+		new LwjglApplication(new Game(), config);
+	}
+}
