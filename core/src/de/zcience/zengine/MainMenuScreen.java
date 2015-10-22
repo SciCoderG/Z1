94d989b2e06906facc9b6ea36ff289681bb348d9
Zcience <david.liebemann@gmail.com>
2015-10-22 22:38:48 +0200

    Added FontUtility Added FontUtility to be used in Main Menu You can add BitmapFonts to the FontUtility and request them by a String identifier.

--- core/src/de/zcience/zengine/MainMenuScreen.java
+++ core/src/de/zcience/zengine/MainMenuScreen.java
@@ -4,62 +4,78 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class MainMenuScreen implements Screen{
+public class MainMenuScreen implements Screen
+{
 
-	private final ZApplication game;
-	private OrthographicCamera camera;
-	
-	
-	public MainMenuScreen(ZApplication game) {
-		this.game = game;
-		
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-	}
-	
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
+    private final ZApplication game;
 
-	@Override
-	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+    private Viewport viewPort;
+
+    // TODO: This is only for testing!
+    private BitmapFont testingFont;
+
+    public MainMenuScreen(ZApplication game)
+    {
+        this.game = game;
+
+        viewPort = new ScreenViewport();
+
+        testingFont = game.getFontUtility().getFont("MainMenuFont");
+
+    }
+
+    @Override
+    public void show()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render(float delta)
+    {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        camera.update();
-        
-	}
 
-	@Override
-	public void resize(int width, int height) {
-		camera.setToOrtho(false, width, height);
-	}
+        game.getBatch().begin();
+        testingFont.draw(game.getBatch(), "heyho", 100, 100);
+        game.getBatch().end();
+    }
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resize(int width, int height)
+    {
+        viewPort.update(width, height);
+    }
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pause()
+    {
+        // TODO Auto-generated method stub
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resume()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void hide()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose()
+    {
+        testingFont.dispose();
+    }
 
 }
--- core/src/de/zcience/zengine/ZApplication.java
+++ core/src/de/zcience/zengine/ZApplication.java
@@ -6,32 +6,63 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ZApplication extends Game {
-	private SpriteBatch batch;
-	public BitmapFont font;
-	
-	
-	@Override
-	public void create() {
-		// Setting LogLevel. For now: Log everything
-		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+import de.zcience.zengine.fonts.FontUtility;
 
-		batch = new SpriteBatch();
-		font = new BitmapFont();
-		// Create an AssetManager, create a Loader Class for Screens to make it all Data-Driven
-	}
+public class ZApplication extends Game
+{
+    private SpriteBatch batch;
 
-	public SpriteBatch getBatch() {
-		return batch;
-	}
+    private FontUtility fontUtility;
 
-	public void setBatch(SpriteBatch batch) {
-		this.batch = batch;
-	}
+    @Override
+    public void create()
+    {
+        // Setting LogLevel. For now: Log everything
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        
+        // set clear color
+        Gdx.gl.glClearColor(0, 0, 1.0f, 1.0f);
 
-	@Override
-	public void render() {
-		super.render(); // Important! Needed, because we're extending the gdx
-						// Game class
-	}
+        
+        batch = new SpriteBatch();
+        fontUtility = new FontUtility();
+        // Create an AssetManager, create a Loader Class for Screens to make it
+        // all Data-Driven
+        this.setScreen(new MainMenuScreen(this));
+
+    }
+
+    public SpriteBatch getBatch()
+    {
+        return batch;
+    }
+
+    public void setBatch(SpriteBatch batch)
+    {
+        this.batch = batch;
+    }
+
+    public FontUtility getFontUtility()
+    {
+        return fontUtility;
+    }
+
+    public void setFontUtility(FontUtility fontUtility)
+    {
+        this.fontUtility = fontUtility;
+    }
+
+    @Override
+    public void render()
+    {
+        super.render(); // Important! Needed, because we're extending the gdx
+                        // Game class
+    }
+    
+    @Override
+    public void dispose()
+    { 
+        super.dispose();
+        fontUtility.dispose();
+    }
 }
