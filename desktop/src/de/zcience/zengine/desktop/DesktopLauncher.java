d4d0b4c3ac1b7520bfef42c626339f042f55067d
Zcience <david.liebemann@gmail.com>
2015-10-27 02:22:42 +0100

    Fixed the gradle setup Got rid of the com.kotcrab.vis.vis-ui package for the gwt setup It was impossible to write the json file...

--- desktop/src/de/zcience/zengine/desktop/DesktopLauncher.java
+++ desktop/src/de/zcience/zengine/desktop/DesktopLauncher.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.zcience.zengine.ZApplication;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "z1";
-		config.width=480;
-		config.height= config.width * 9 / 16;
-		new LwjglApplication(new ZApplication(), config);
-	}
+public class DesktopLauncher
+{
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "z1";
+        config.width = 480;
+        config.height = config.width * 9 / 16;
+        new LwjglApplication(new ZApplication(), config);
+    }
 }
