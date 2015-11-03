fb9e4c78e71b9039588561b3f115a3454289f491
Zcience <david.liebemann@gmail.com>
2015-11-03 17:04:29 +0100

    Initialised Box2D and added Box2D world Still have to do integrating and think about usage of the Box2D bodies. If we will use them directly at all...

--- core/src/de/zcience/zengine/physics/PhysicsSystem.java
+++ core/src/de/zcience/zengine/physics/PhysicsSystem.java
@@ -2,7 +2,10 @@ package de.zcience.zengine.physics;
 
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
-import com.badlogic.ashley.systems.IntervalIteratingSystem;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 
 /**
  * Physicssystem updating the box2d world. Should have a set Intervall to ensure
@@ -11,17 +14,34 @@ import com.badlogic.ashley.systems.IntervalIteratingSystem;
  * @author David_000
  *
  */
-public class PhysicsSystem extends IntervalIteratingSystem {
+public class PhysicsSystem extends IteratingSystem {
 
-	public PhysicsSystem(Family family, float interval) {
-		super(family, interval);
-		// TODO Auto-generated constructor stub
+	private World world;
+	
+	
+
+	public PhysicsSystem(Vector2 gravity, boolean doSleep, double dt) {
+		super(Family.all(PhysicsComponent.class).get());
+		
+		Box2D.init();
+		this.world = new World(gravity, doSleep);
 	}
 
 	@Override
-	protected void processEntity(Entity entity) {
+	public void update(float deltaTime) {
+
+		super.update(deltaTime);
+	}
+
+	@Override
+	protected void processEntity(Entity entity, float deltaTime) {
 		// TODO Auto-generated method stub
 
 	}
 
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
 }
